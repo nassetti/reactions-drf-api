ab7e860ac5f7b11634f0861a8486c53c9517053d
nassetti <nassettimax@gmail.com>
2022-11-29 02:27:34 +0000

    Create Profiles model.

---
+++ profiles/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    content = models.CharField(max_length=300, blank=False)
+    profile_image = models.ImageField(
+        upload_to='images/', default='../blank_profile_kgtnqf'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
