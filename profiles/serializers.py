31ddc3b63dd4113377020f4bb923c93ab7cc957a
nassetti <nassettimax@gmail.com>
2022-11-29 03:27:01 +0000

    Add permissions based access.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,10 +4,15 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'first_name',
-            'last_name', 'content', 'profile_image'
+            'last_name', 'content', 'profile_image', 'is_owner'
         ]
